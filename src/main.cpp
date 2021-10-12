ba18b92a8eeea0b7a419b46bb11b74964d1ee96d
Sujay Chand <sujaygchand@gmail.com>
2021-10-12 17:20:48 +1300

    Set window size and title

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,10 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     ApplicationManager window;
+
+    window.resize(450,600);
+    window.setWindowTitle("Breakout");
     window.show();
+
     return a.exec();
 }
